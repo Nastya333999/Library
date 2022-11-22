d03e000153bef2c7beb740b7fda6c8b116e8110c
a.dmytriieva.a <a.dmytriieva.a@gmail.com>
2022-11-22 14:11:52 +0200

    0.0.1

--- app/src/main/java/com/app/library/MainActivity.kt
+++ app/src/main/java/com/app/library/MainActivity.kt
@@ -2,15 +2,22 @@ package com.app.library
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.room.Room
 import com.app.mylibrary.AppDatabase
 
 class MainActivity : AppCompatActivity() {
+    lateinit var dataBase : AppDatabase
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 
-        
+            dataBase = Room.databaseBuilder(
+                this,
+                AppDatabase::class.java, "DataUrl"
+            ).build()
+        dataBase.dataUrlDao()
     }
 
 
